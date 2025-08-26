8028edcb4da13cef5517c0946d4ca48e5b829055
Konfus-dev <63468146+Konfus-dev@users.noreply.github.com>
2025-08-26 16:41:13 -0700

    Implement SDL input code converters and handler logic
    Added SDLTbxInputCodeConverters.h to map TBX input codes to SDL equivalents. Refactored SDLInputHandler to use these converters for keyboard, mouse, and gamepad input queries. Improved gamepad management with initialization and cleanup in constructor/destructor, and updated input state methods for all device types.

--- Source/SDLInputHandler.cpp
+++ Source/SDLInputHandler.cpp
@@ -1,11 +1,41 @@
 #include "SDLInputHandler.h"
+#include "SDLTbxInputCodeConverters.h"
+#include <Tbx/Events/EventCoordinator.h>
 #include <Tbx/Debug/Debugging.h>
 
 namespace SDLInput
 {
+    /* ==== Initialization ==== */
+
+    SDLInputHandler::SDLInputHandler()
+    {
+        SDL_InitSubSystem(SDL_INIT_GAMEPAD);
+
+        // Open all connected gamepads
+        int numGamepads = 0;
+        SDL_GetGamepads(&numGamepads);
+        for (int i = 0; i < numGamepads; ++i)
+        {
+            if (SDL_IsGamepad(i))
+            {
+                _gamepads[i]= SDL_OpenGamepad(i);
+            }
+        }
+    }
+
+    SDLInputHandler::~SDLInputHandler()
+    {
+        for (auto& [id, gamepad] : _gamepads)
+        {
+            if (gamepad)
+            {
+                SDL_CloseGamepad(gamepad);
+            }
+        }
+    }
+
     void SDLInputHandler::OnLoad()
     {
-        TBX_ASSERT(SDL_Init(SDL_INIT_EVENTS) != 0, "Failed to initialize SDL");
         TBX_ASSERT(SDL_Init(SDL_INIT_GAMEPAD) != 0, "Failed to initialize SDL");
         TBX_ASSERT(SDL_Init(SDL_INIT_HAPTIC) != 0, "Failed to initialize SDL");
         TBX_ASSERT(SDL_Init(SDL_INIT_SENSOR) != 0, "Failed to initialize SDL");
@@ -13,59 +43,100 @@ namespace SDLInput
 
     void SDLInputHandler::OnUnload()
     {
+        SDL_QuitSubSystem(SDL_INIT_GAMEPAD);
+        SDL_QuitSubSystem(SDL_INIT_HAPTIC);
+        SDL_QuitSubSystem(SDL_INIT_SENSOR);
     }
 
     void SDLInputHandler::SetContext(const std::shared_ptr<Tbx::IWindow>& windowToListenTo)
     {
+        // Do nothing...
     }
 
-    bool SDLInputHandler::IsGamepadButtonDown(const int gamepadId, const int button) const
-    {
-        return false;
-    }
-
-    bool SDLInputHandler::IsGamepadButtonUp(const int gamepadId, const int button) const
-    {
-        return false;
-    }
-
-    bool SDLInputHandler::IsGamepadButtonHeld(const int gamepadId, const int button) const
-    {
-        return false;
-    }
+    /* ==== Keyboard ==== */
 
     bool SDLInputHandler::IsKeyDown(const int keyCode) const
     {
-        return false;
+        const auto* keyboardState = SDL_GetKeyboardState(nullptr);
+        if (keyboardState == nullptr) return false;
+        SDL_Scancode sc = ConvertKey(keyCode);
+        return keyboardState[sc];
     }
 
     bool SDLInputHandler::IsKeyUp(const int keyCode) const
     {
-        return false;
+        const auto* keyboardState = SDL_GetKeyboardState(nullptr);
+        if (keyboardState == nullptr) return false;
+        SDL_Scancode sc = ConvertKey(keyCode);
+        return !keyboardState[sc];
     }
 
     bool SDLInputHandler::IsKeyHeld(const int keyCode) const
     {
-        return false;
+        const auto* keyboardState = SDL_GetKeyboardState(nullptr);
+        if (keyboardState == nullptr) return false;
+        SDL_Scancode sc = ConvertKey(keyCode);
+        return keyboardState[sc];
     }
+
+    /* ==== Mouse ==== */
 
     bool SDLInputHandler::IsMouseButtonDown(const int button) const
     {
-        return false;
+        Uint32 mask = SDL_GetMouseState(nullptr, nullptr);
+        int sdlBtn = ConvertMouseButton(button);
+        return (mask & SDL_BUTTON_MASK(sdlBtn));
     }
 
     bool SDLInputHandler::IsMouseButtonUp(const int button) const
     {
-        return false;
+        Uint32 mask = SDL_GetMouseState(nullptr, nullptr);
+        int sdlBtn = ConvertMouseButton(button);
+        return !(mask & SDL_BUTTON_MASK(sdlBtn));
     }
 
     bool SDLInputHandler::IsMouseButtonHeld(const int button) const
     {
-        return false;
+        Uint32 mask = SDL_GetMouseState(nullptr, nullptr);
+        int sdlBtn = ConvertMouseButton(button);
+        return (mask & SDL_BUTTON_MASK(sdlBtn));
     }
 
     Tbx::Vector2 SDLInputHandler::GetMousePosition() const
     {
-        return Tbx::Vector2();
+        float x, y;
+        SDL_GetMouseState(&x, &y);
+        return Tbx::Vector2(x, y);
+    }
+
+    /* ==== Gamepads ==== */
+
+    bool SDLInputHandler::IsGamepadButtonDown(const int gamepadId, const int button) const
+    {
+        auto it = _gamepads.find(gamepadId);
+        if (it == _gamepads.end() || !it->second) return false;
+
+        int sdlBtn = ConvertGamepadButton(button);
+        bool curr = SDL_GetGamepadButton(it->second, (SDL_GamepadButton)sdlBtn);
+        return curr;
+    }
+
+    bool SDLInputHandler::IsGamepadButtonUp(const int gamepadId, const int button) const
+    {
+        auto it = _gamepads.find(gamepadId);
+        if (it == _gamepads.end() || !it->second) return false;
+
+        int sdlBtn = ConvertGamepadButton(button);
+        bool curr = SDL_GetGamepadButton(it->second, (SDL_GamepadButton)sdlBtn);
+        return !curr;
+    }
+
+    bool SDLInputHandler::IsGamepadButtonHeld(const int gamepadId, const int button) const
+    {
+        auto it = _gamepads.find(gamepadId);
+        if (it == _gamepads.end() || !it->second) return false;
+
+        int sdlBtn = ConvertGamepadButton(button);
+        return SDL_GetGamepadButton(it->second, (SDL_GamepadButton)sdlBtn);
     }
 }
--- Source/SDLInputHandler.h
+++ Source/SDLInputHandler.h
@@ -1,26 +1,37 @@
 #pragma once
 #include <SDL3/SDL.h>
+#include <Tbx/Events/AppEvents.h>
 #include <Tbx/PluginAPI/RegisterPlugin.h>
+#include <unordered_map>
 
 namespace SDLInput
 {
     class SDLInputHandler : public Tbx::IInputHandlerPlugin
     {
     public:
+        SDLInputHandler();
+        ~SDLInputHandler();
+
         void OnLoad() override;
         void OnUnload() override;
 
         void SetContext(const std::shared_ptr<Tbx::IWindow>& windowToListenTo) override;
+
         bool IsGamepadButtonDown(const int gamepadId, const int button) const override;
         bool IsGamepadButtonUp(const int gamepadId, const int button) const override;
         bool IsGamepadButtonHeld(const int gamepadId, const int button) const override;
+
         bool IsKeyDown(const int keyCode) const override;
         bool IsKeyUp(const int keyCode) const override;
         bool IsKeyHeld(const int keyCode) const override;
+
         bool IsMouseButtonDown(const int button) const override;
         bool IsMouseButtonUp(const int button) const override;
         bool IsMouseButtonHeld(const int button) const override;
-        Tbx::Vector2 GetMousePosition() const override;
+        Tbx::Vector2 GetMousePosition() const override; 
+    
+    private:
+        std::unordered_map<int, SDL_Gamepad*> _gamepads = {};
     };
 
     TBX_REGISTER_PLUGIN(SDLInputHandler);
